0016069fd3c30b8b49356b85e7b7e582ed33c02c
mateuszkalinowski <mateusz.kalinowski@icloud.com>
2016-10-08 18:37:36 +0200

    Wersja gry 0.9.6 Poprawianie zdolnosci gracza komputerowego...

--- src/logic/ComputerPlayer.java
+++ src/logic/ComputerPlayer.java
@@ -31,6 +31,97 @@ public class ComputerPlayer {
         }
     }
 
+    private boolean isFourRowHorizontallyStrict(int x1,int y1,int x2,int y2) {
+        if(y1!=y2) return false;
+        int tmp = 0;
+        if(x1>x2) {
+            tmp = x1;
+            x2 = x1;
+            x1 = tmp;
+        }
+        int y = y1;
+        try{
+            if(board.board[x1-1][y]!=0 || board.board[x2+1][y]!=0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+            return false;
+        }
+
+        try{
+            if(board.board[x1-2][y]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x2+2][y]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+
+        try{
+            if(board.board[x1-1][y-1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x1][y-1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x2][y-1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x2+1][y-1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+
+
+        try{
+            if(board.board[x1-1][y+1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x1][y+1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x2][y+1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        try{
+            if(board.board[x2+1][y+1]==0)
+                return false;
+        }catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+
+
+
+        return true;
+    }
+
+   private boolean isFourRowVerticallyStrict(int x1,int y1,int x2,int y2) {
+       /* if (x1 != x2) return false;
+        int tmp = 0;
+        if (y1 > y2) {
+            tmp = y1;
+            y2 = y1;
+            y1 = tmp;
+        }
+        int x = x1;
+*/
+
+
+
+        return false;
+    }
+
     private boolean findFourRowHorizontallyStrict() {
         for (int i = 0; i < board.staticSize - 3; i++) {
             for (int j = 0; j < board.staticSize; j++) {
@@ -194,32 +285,40 @@ public class ComputerPlayer {
 
         if (board.board[x][y] == 0) {
             if (direction == 0 && directions[0]) {
-                board.board[x][y] = 2;
-                board.board[x][y - 1] = 2;
-                movesStorage.addMove(x,y,x,y-1);
-           //     System.out.println("-----------------");
-                return true;
+                if(!isFourRowHorizontallyStrict(x,y,x,y-1) && !isFourRowVerticallyStrict(x,y,x,y-1)) {
+                    board.board[x][y] = 2;
+                    board.board[x][y - 1] = 2;
+                    movesStorage.addMove(x, y, x, y - 1);
+                    //     System.out.println("-----------------");
+                    return true;
+                }
             }
             if (direction == 1 && directions[1]) {
-                board.board[x][y] = 2;
-                board.board[x + 1][y] = 2;
-                movesStorage.addMove(x,y,x+1,y);
-          //      System.out.println("-----------------");
-                return true;
+                if(!isFourRowHorizontallyStrict(x,y,x+1,y) && !isFourRowVerticallyStrict(x,y,x+1,y)) {
+                    board.board[x][y] = 2;
+                    board.board[x + 1][y] = 2;
+                    movesStorage.addMove(x, y, x + 1, y);
+                    //      System.out.println("-----------------");
+                    return true;
+                }
             }
             if (direction == 2 && directions[2]) {
-                board.board[x][y] = 2;
-                board.board[x][y + 1] = 2;
-                movesStorage.addMove(x,y,x,y+1);
-          //      System.out.println("-----------------");
-                return true;
+                if(!isFourRowHorizontallyStrict(x,y,x,y+1) && !isFourRowVerticallyStrict(x,y,x,y+1)) {
+                    board.board[x][y] = 2;
+                    board.board[x][y + 1] = 2;
+                    movesStorage.addMove(x, y, x, y + 1);
+                    //      System.out.println("-----------------");
+                    return true;
+                }
             }
             if (direction == 3 && directions[3]) {
-                board.board[x][y] = 2;
-                board.board[x - 1][y] = 2;
-                movesStorage.addMove(x,y,x-1,y);
-            //    System.out.println("-----------------");
-                return true;
+                if(!isFourRowHorizontallyStrict(x,y,x-1,y) && !isFourRowVerticallyStrict(x,y,x-1,y)) {
+                    board.board[x][y] = 2;
+                    board.board[x - 1][y] = 2;
+                    movesStorage.addMove(x, y, x - 1, y);
+                    //    System.out.println("-----------------");
+                    return true;
+                }
             }
         }
         return false;
